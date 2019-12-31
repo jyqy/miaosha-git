461b8692ef5f560ab510d5b390cb847bb376d86c
jyqy <605517875@qq.com>
2020-01-01 00:43:32 +0800

    miaosha_3

---
+++ miaosha_3/src/main/java/com/gpnu/miaosha/domain/Goods.java
@@ -0,0 +1,11 @@
+package com.gpnu.miaosha.domain;
+
+public class Goods {
+    private  Long id;
+    private String goodsName;
+    private String goodsTitle;
+    private String goodsImg;
+    private String gooodsDetail;
+    private Double goodsPrice;
+
+}
